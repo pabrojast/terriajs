e0ef8d4b77add07a958be6f157d1839baac9a45f
CAZALAC <pablo.ignacio.rt@gmail.com>
2025-09-03 14:44:35 -0400

    Refactor: Adjust right positioning of SentinelCreditOverlay for improved layout

--- lib/ReactViews/Map/BottomBar/Credits/SentinelCreditOverlay.tsx
+++ lib/ReactViews/Map/BottomBar/Credits/SentinelCreditOverlay.tsx
@@ -20,7 +20,7 @@ const SentinelCreditContainer = styled(Box).attrs(() => ({
   /* Adjust position based on timeline visibility */
   bottom: ${(props) => (props.isTimelineActive ? "110px" : "36px")};
   left: 0;
-  right: 40%; /* Limit width so it doesn't span too far across the screen */
+  right: 70%; /* Limit width so it doesn't span too far across the screen */
   background: ${(props) => props.theme.transparentDark};
   backdrop-filter: ${(props) => props.theme.blur};
   font-size: 0.7rem;
