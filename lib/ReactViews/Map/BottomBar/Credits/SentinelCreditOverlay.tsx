5804f5e85ae2fc6262fbba46e1c869e6c7b63911
CAZALAC <pablo.ignacio.rt@gmail.com>
2025-09-03 16:43:15 -0400

    Refactor: Adjust right positioning of BasemapCreditOverlay for improved layout

--- lib/ReactViews/Map/BottomBar/Credits/SentinelCreditOverlay.tsx
+++ lib/ReactViews/Map/BottomBar/Credits/SentinelCreditOverlay.tsx
@@ -20,7 +20,7 @@ const BasemapCreditContainer = styled(Box).attrs(() => ({
   /* Adjust position based on timeline visibility */
   bottom: ${(props) => (props.isTimelineActive ? "110px" : "36px")};
   left: 0;
-  right: 30%; /* Limit width so it doesn't span too far across the screen */
+  right: 20%; /* Limit width so it doesn't span too far across the screen */
   background: ${(props) => props.theme.transparentDark};
   backdrop-filter: ${(props) => props.theme.blur};
   font-size: 0.7rem;
